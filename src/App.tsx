d5700fec248a5da1a1d3e3d4cd3739d5e28858f8
JDAAR <84633989+JDDAR@users.noreply.github.com>
2026-09-02 18:23:07 -0500

    feat: estructura base del proyecto (vite + react + ts + reveal)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <h1>Presentaciòn conflictos</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
